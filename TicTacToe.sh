5583e6576deb7905a99387fa33aacfe6dbaeabff
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-24 10:06:28 +0530

    ADDED:Function To Reset The Game Board So To Start Fresh

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,3 +2,29 @@
 
 #DISPLAYING WELCOME MESSAGE
 echo "Welcome To Tic Tac Toe Game"
+
+#DECLARING ARRAY
+declare -a gameBoard
+
+#CONSTANTS
+NUMBER_OF_ROWS=3;
+NUMBER_OF_COLUMNS=3;
+
+#VARIABLES
+rows=0;
+columns=0;
+
+#FUNCTION TO RESET THE GAME BOARD
+function resetGameBoard() {
+	for (( rows=1; rows<=NUMBER_OF_ROWS; rows++ ))
+	do
+		for(( columns=1; columns<=NUMBER_OF_COLUMNS; columns++ ))
+		do
+			gameBord[$rows,$columns]=""
+		done
+	done
+}
+
+#CALLING THE FUNCTION TO RESET THE BOARD
+resetGameBoard
+
