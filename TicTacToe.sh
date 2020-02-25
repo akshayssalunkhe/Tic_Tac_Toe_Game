5b3be36f325804d718a41805162aafa884e1ad9d
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-02-25 17:45:52 +0530

    ADDED:Functions To Check Winning Conditions

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -3,25 +3,27 @@
 #DISPLAYING WELCOME MESSAGE
 echo "Welcome To Tic Tac Toe Game"
 
-#DECLARING ARRAY
-declare -a gameBoard
+#DECLARING DICTIONARY
+declare -A gameBoard
 
 #CONSTANTS
-NUMBER_OF_ROWS=3;
-NUMBER_OF_COLUMNS=3;
 ZERO=0;
+SIZE=3;
+TOTAL_TURN=9;
 
 #VARIABLES
 rows=0;
+count=0;
 columns=0;
 userSymbol=0;
-counter=1;
+rowPosition=0;
+columnPosition=0;
 
 #FUNCTION TO RESET THE GAME BOARD
 function resetGameBoard() {
-	for (( rows=1; rows<=NUMBER_OF_ROWS; rows++ ))
+	for (( rows=1; rows<=$SIZE; rows++ ))
 	do
-		for(( columns=1; columns<=NUMBER_OF_COLUMNS; columns++ ))
+		for(( columns=1; columns<=$SIZE; columns++ ))
 		do
 			gameBord[$rows,$columns]=""
 		done
@@ -35,11 +37,13 @@ resetGameBoard
 function assignSymbol() {
 	if [[ $((RANDOM%2)) -eq 0 ]]
 	then
-		echo "Assigned Symbol To User Is 'O' "
-		userSymbol=O;
+		echo "Assigned Symbol To Player One Is 'O' "
+		playerOneSymbol=O;
+		playerTwoSymbol=X;
 	else
-		echo "Assigned Symbol To User Is 'X' "
-		userSymbol=X;
+		echo "Assigned Symbol To Player One Is 'X' "
+		playerOneSymbol=X;
+		playerTwoSymbol=O;
 	fi
 }
 
@@ -50,9 +54,9 @@ assignSymbol
 function whoPlaysFirst() {
 	if [[ $((RANDOM%2)) -eq $ZERO ]]
 	then
-		echo "User Plays First"
+		echo "Player One Plays First"
 	else
-		echo "Computer Plays First"
+		echo "Player Two Plays First"
 	fi
 }
 
@@ -61,19 +65,96 @@ whoPlaysFirst
 
 #FUNCTION TO DISPLAY GAME BOARD
 function displayGameBoard() {
-	for(( rows=1; rows<=NUMBER_OF_ROWS; rows++ ))
+	for (( rows=1; rows<=$SIZE; rows++ ))
 	do
-		echo "---------------"
-		for(( columns=1; columns<=NUMBER_OF_COLUMNS; columns++ ))
+		echo "-------------------------"
+		for (( columns=1; columns<=$SIZE; columns++ ))
 		do
-			echo -n " $counter  |"
-			((counter++))
+			echo -ne "${gameBoard[$rows,$columns]}\t|"
 		done
 		echo  ""
 	done
-	echo "---------------"
+	echo "-------------------------"
 }
 
 #CALLING FUNCTION TO DISPLAY GAME BOARD
 displayGameBoard
 
+#FUNCTION TO CHECK WINNING ROW
+function isWinnerRow() {
+	for (( rows=1; rows<=$SIZE; rows++ ))
+	do
+		for (( columns=1; columns<=$SIZE; columns++))
+		do
+			if [[ ${gameBoard[$rows,$columns]} == ${gameBoard[$rows,$(($columns+1))]} ]] &&
+				[[ ${gameBoard[$rows,$(($columns+1))]} == ${gameBoard[$rows,$(($columns+2))]} ]] &&
+				[[ ${gameBoard[$rows,$columns]}  ]]
+			then
+				echo  "WinRows"
+				exit
+			fi
+		done
+	done
+}
+
+#FUNCTION TO CHECK WINNING COLUMN
+function isWinnerColumn() {
+	for (( rows=1; rows<=$SIZE; rows++ ))
+	do
+		for (( columns=1; columns<=$SIZE; columns++ ))
+		do
+			if [[ ${gameBoard[$rows,$columns]} == ${gameBoard[$(($rows+1)),$columns]} ]] &&
+				[[ ${gameBoard[$(($rows+1)),$columns]} == ${gameBoard[$(($rows+2)),$columns]} ]] &&
+				[[ ${gameBoard[$rows,$columns]} ]]
+			then
+				echo  "Wincolumns"
+				exit
+			fi
+		done
+	done
+}
+
+#FUNCTION TO CHECK WINNING DIADONAL
+function winnerDiagonal() {
+	if [[ ${gameBoard[1,1]} == ${gameBoard[2,2]} ]] && [[ ${gameBoard[2,2]} == ${gameBoard[3,3]} ]] && [[ ${gameBoard[1,1]} ]]
+	then
+		echo "Win Diagonal"
+		exit
+	elif [[ ${gameBoard[1,3]} == ${gameBoard[2,2]} ]] && [[ ${gameBoard[2,2]} == ${gameBoard[3,1]} ]] && [[ ${gameBoard[1,3]} ]]
+	then
+		echo  "Win Diagonal"
+		exit
+	fi
+}
+
+#CALLING FUNCTION ASSIGN SYMBOL
+assignSymbol
+
+function playTicTacToe() {
+	for (( input=1; input<=$TOTAL_TURN; input++ ))
+	do
+		if [[ $count -eq $ZERO ]]
+		then
+			read -p "Player One Turn Enter The Row Position And Column Position Respectively " rowPosition columnPosition
+			gameBoard[$rowPosition,$columnPosition]=$playerOneSymbol
+			((count++))
+			displayGameBoard
+			isWinnerRow
+			isWinnerColumn
+			winnerDiagonal
+		else
+			read -p "Player Two Turn Enter The Row Position And Column Position Respectively " rowPosition columnPosition
+			gameBoard[$rowPosition,$columnPosition]=$playerTwoSymbol
+			displayGameBoard
+			isWinnerRow
+			isWinnerColumn
+			winnerDiagonal
+			count=0;
+		fi
+		if [[ $input == $TOTAL_TURN ]]
+		then
+			echo "It's Tie"
+		fi
+	done
+}
+playTicTacToe
